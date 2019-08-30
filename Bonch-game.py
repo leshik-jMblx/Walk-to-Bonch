a039980e971678aad95431fa0123f2bfe0e525ad
leshik-jMblx <bondarchuckaleksey@yandex.ru>
2019-08-31 00:53:27 +0300

    main menu

--- Bonch-game.py
+++ Bonch-game.py
@@ -3,7 +3,7 @@
 import pygame, sys
 
 pygame.init()
-main_window = pygame.display.set_mode((900, 600))
+main_window = pygame.display.set_mode((1300, 800))
 pygame.display.set_caption('First Game (test)')
 main_window.fill((100, 150, 200))
 # Colors
@@ -14,15 +14,40 @@ grad_color = (128, 128, 128)
 yellow_color = (255, 255, 0)
 
 
+# MAIN MENU ------------------------------------------------ MAIN MENU
+def main_menu():
+	main_window.fill((0, 0, 0))
+	
+	press = pygame.image.load('press.png')
+	press_pos = press.get_rect(center=(560, 430))
+	main_window.blit(press,press_pos)
+	
+	enter = pygame.image.load('enter.png')
+	enter_pos = enter.get_rect(center=(760, 430))
+	main_window.blit(enter,enter_pos)	
+	
+	bonc = pygame.image.load('bonc.png')
+	bonc_pos = bonc.get_rect(center=(475, 150))
+	main_window.blit(bonc,bonc_pos)	
+	
+	hman = pygame.image.load('hman.png')
+	hman_pos = hman.get_rect(center=(720, 150))
+	main_window.blit(hman,hman_pos)	
+	
+	smile = pygame.image.load('))).png')
+	smile_pos = smile.get_rect(center=(970, 155))
+	main_window.blit(smile,smile_pos)	
+
+
 
 
 def paintWindow():
-	main_window.fill((0, 0, 0))
+	main_window.fill((0, 0, 0))	
 	
 	# Road-line and purple border		
 	pygame.draw.line(main_window, white_color, [6, 400], [892, 400], 4)
 	#pygame.draw.rect(main_window, grad_color, (6, 403, 892, 25))
-	pygame.draw.rect(main_window, purple_color, (3, 3, 894, 594), 5)
+	
 
 	# Houses
 	pygame.draw.lines(main_window, gray_color, False,\
@@ -67,9 +92,19 @@ while True:
 	pygame.time.delay(50)
 	
 	for k in pygame.event.get():
+		if k.type == pygame.KEYUP:
+			if k.key == pygame.K_RETURN:
+				sys.exit()
+				
 		if k.type == pygame.QUIT:
 			sys.exit()
 	
-	paintWindow()
+	main_menu()	
+
+	
+	
+	#paintWindow()
+	
+ 
 			
 	pygame.display.update()
